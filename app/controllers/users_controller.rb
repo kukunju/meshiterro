2a08ed8da914adff6f83613756acd6cc47da5e86
kukunju <132256077+kukunju@users.noreply.github.com>
2023-05-15 09:47:19 +0000

    [Finish]4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-    @post.images = @user.post_images
+    @post_images = @user.post_images
   end
 
   def edit
   end
-  
+
 end
